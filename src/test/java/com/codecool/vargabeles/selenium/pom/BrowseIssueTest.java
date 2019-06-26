6868e1337b11df877f52d0f3357d8e08804889fe
Bence <arvai.bence1@gmail.com>
2019-06-26 15:06:50 +0200

    Use CSV file for Browse Issue tests

--- src/test/java/com/codecool/vargabeles/selenium/pom/BrowseIssueTest.java
+++ src/test/java/com/codecool/vargabeles/selenium/pom/BrowseIssueTest.java
@@ -1,9 +1,10 @@
 package com.codecool.vargabeles.selenium.pom;
 
 import com.codecool.vargabeles.selenium.pom.pageObjects.IssuePage;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,28 +18,9 @@ public class BrowseIssueTest extends BaseTest{
         assertTrue(issuePage.getPageTitle().contains("SAND-40"));
     }
 
-    //TODO separate?
     @ParameterizedTest
-    @ValueSource(strings = {"JETI-1", "JETI-2","JETI-3"})
-    void projectJetiHasThreeIssuesAndIndexedProperly(String issueTitle){
-        IssuePage issuePage = new IssuePage(driver);
-
-        issuePage.navigate("/browse/" + issueTitle);
-        assertTrue(issuePage.getPageTitle().contains(issueTitle));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"TOUCAN-1", "TOUCAN-2","TOUCAN-3"})
-    void projectToucanHasThreeIssuesAndIndexedProperly(String issueTitle){
-        IssuePage issuePage = new IssuePage(driver);
-
-        issuePage.navigate("/browse/" + issueTitle);
-        assertTrue(issuePage.getPageTitle().contains(issueTitle));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"COALA-1", "COALA-2","COALA-3"})
-    void projectCoalaHasThreeIssuesAndIndexedProperly(String issueTitle){
+    @CsvFileSource(resources = "/issueTitles.csv",numLinesToSkip = 1)
+    void everyProjectHasThreeIssuesAndIndexedProperly(String issueTitle){
         IssuePage issuePage = new IssuePage(driver);
 
         issuePage.navigate("/browse/" + issueTitle);
