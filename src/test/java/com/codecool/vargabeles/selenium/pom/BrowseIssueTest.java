07350f086559697b20b047f3915d5949e3dd1b40
Bence <arvai.bence1@gmail.com>
2019-06-26 13:21:52 +0200

    Add tests for 3 issues (indexed 1-3) per project

--- src/test/java/com/codecool/vargabeles/selenium/pom/BrowseIssueTest.java
+++ src/test/java/com/codecool/vargabeles/selenium/pom/BrowseIssueTest.java
@@ -12,7 +12,50 @@ public class BrowseIssueTest extends BaseTest{
         IssuePage issuePage = new IssuePage(driver);
 
         issuePage.navigate("/browse/SAND-40");
-        assertTrue(issuePage.getPageTitle().contains("[SAND-40]"));
+        assertTrue(issuePage.getPageTitle().contains("SAND-40"));
+    }
+
+    //TODO separate?
+    @Test
+    void projectJetiHasThreeIssuesAndIndexedProperly(){
+        IssuePage issuePage = new IssuePage(driver);
+
+        issuePage.navigate("/browse/JETI-1");
+        assertTrue(issuePage.getPageTitle().contains("JETI-1"));
+
+        issuePage.navigate("/browse/JETI-2");
+        assertTrue(issuePage.getPageTitle().contains("JETI-2"));
+
+        issuePage.navigate("/browse/JETI-3");
+        assertTrue(issuePage.getPageTitle().contains("JETI-3"));
+    }
+
+    @Test
+    void projectToucanHasThreeIssuesAndIndexedProperly(){
+        IssuePage issuePage = new IssuePage(driver);
+
+        issuePage.navigate("/browse/TOUCAN-1");
+        assertTrue(issuePage.getPageTitle().contains("TOUCAN-1"));
+
+        issuePage.navigate("/browse/TOUCAN-2");
+        assertTrue(issuePage.getPageTitle().contains("TOUCAN-2"));
+
+        issuePage.navigate("/browse/TOUCAN-3");
+        assertTrue(issuePage.getPageTitle().contains("TOUCAN-3"));
+    }
+
+    @Test
+    void projectCoalaHasThreeIssuesAndIndexedProperly(){
+        IssuePage issuePage = new IssuePage(driver);
+
+        issuePage.navigate("/browse/COALA-1");
+        assertTrue(issuePage.getPageTitle().contains("COALA-1"));
+
+        issuePage.navigate("/browse/COALA-2");
+        assertTrue(issuePage.getPageTitle().contains("COALA-2"));
+
+        issuePage.navigate("/browse/COALA-3");
+        assertTrue(issuePage.getPageTitle().contains("COALA-3"));
     }
 }
 
