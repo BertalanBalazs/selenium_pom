7a2d217ed3ec12da1d984782f5f0b1d0bb94413e
Bence <arvai.bence1@gmail.com>
2019-06-27 09:54:56 +0200

    HOTFIX: missing login in createdIssueExists test

--- src/test/java/com/codecool/vargabeles/selenium/pom/BrowseIssueTest.java
+++ src/test/java/com/codecool/vargabeles/selenium/pom/BrowseIssueTest.java
@@ -31,9 +31,9 @@ public class BrowseIssueTest extends BaseTest {
 
     @Test
     void createdIssueExists() {
+        loginPage.validLogin(System.getenv("username"), System.getenv("password"));
         IssuePage issuePage = new IssuePage(driver);
         issuePage.navigate("/browse/SAND-40");
-
         assertTrue(issuePage.getPageTitle().contains("SAND-40"));
     }
 
