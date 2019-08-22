42aee509f1f041f8713c74ee74f4555772da411c
Bence <arvai.bence1@gmail.com>
2019-08-22 10:32:04 +0200

    Push test for Jenkins

--- src/main/java/com/codecool/vargabeles/selenium/pom/pageObjects/LoginPage.java
+++ src/main/java/com/codecool/vargabeles/selenium/pom/pageObjects/LoginPage.java
@@ -40,7 +40,7 @@ public class LoginPage extends BasePage {
 
     public String invalidLogin(String username, String password) {
         navigate();
-        //TODO CHANGE THIS TO TEST PUSH TRIGGER
+        //TODO CHANGE THIS TO TEST PUSH TRIGGER 123
         wait.until(ExpectedConditions.visibilityOf(this.userName));
         wait.until(ExpectedConditions.visibilityOf(this.password));
         this.userName.sendKeys(username);
